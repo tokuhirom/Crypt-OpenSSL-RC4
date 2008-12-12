8fb079c3de2e94c6fad6358c20a36a21db7dd935
Tokuhiro Matsuno <tokuhirom@gp.ath.cx>
2008-12-12 12:57:51 +0900

    wrote docs

--- lib/Crypt/OpenSSL/RC4.pm
+++ lib/Crypt/OpenSSL/RC4.pm
@@ -30,21 +30,34 @@ __END__
 
 =head1 NAME
 
-Crypt::OpenSSL::RC4 -
+Crypt::OpenSSL::RC4 - RC4 library based on OpenSSL
 
 =head1 SYNOPSIS
 
-  use Crypt::OpenSSL::RC4;
+    use Crypt::OpenSSL::RC4;
+    # functional style
+    my $encrypted = RC4($passphrase, $plaintext);
+    my $decrypted = RC4($passphrase, $encrypted);
+
+    # OO style
+    my $cipher = Crypt::RC4->new($passphrase);
+    my $encrypted = $cipher->RC4($plain_text);
 
 =head1 DESCRIPTION
 
-Crypt::OpenSSL::RC4 is
+This module is wrapper class for OpenSSL. The interface is compatible with Crypt::RC4.
+
+This module XS implementation of the RC4 algorithm, developed by RSA Security, Inc. Here is the description from Wikipedia website: http://en.wikipedia.org/wiki/RC4
+
+In cryptography, RC4 (also known as ARC4 or ARCFOUR meaning Alleged RC4, see below) is the most widely-used software stream cipher and is used in popular protocols such as Secure Sockets Layer (SSL) (to protect Internet traffic) and WEP (to secure wireless networks). While remarkable for its simplicity and speed in software, RC4 is vulnerable to attacks when the beginning of the output keystream is not discarded, or a single keystream is used twice; some ways of using RC4 can lead to very insecure cryptosystems such as WEP.
 
 =head1 AUTHOR
 
-Default Name E<lt>default {at} example.comE<gt>
+Tokuhiro Matsuno E<lt>tokuhirom.gmailE<gt>
 
 =head1 SEE ALSO
+
+L<Crypt::RC4>, L<Crypt::RC4::XS>
 
 =head1 LICENSE
 
